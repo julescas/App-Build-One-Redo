754507fd8da964d91427c5caf8cb427548e0bf29
julescas <112312841+julescas@users.noreply.github.com>
2022-10-10 01:35:45 -1000

    Initial Commit

---
+++ App Build One Redo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Build One Redo
+//
+//  Created by Julianne Castillo on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ App Build One RedoUITests/App_Build_One_RedoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  App_Build_One_RedoUITestsLaunchTests.swift
+//  App Build One RedoUITests
+//
+//  Created by Julianne Castillo on 10/10/22.
+//
+
+import XCTest
+
+final class App_Build_One_RedoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
